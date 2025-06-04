dc2f0dcfb866231ee7c23bac0aafea92fa5df493
MinJun Park <akio7689@naver.com>
2025-06-04 21:25:26 +0900

    [기능 추가] 한 달 예산이 50만원 경우 예산 관리에 대한 기능 추가

--- OSS_Budget/budget.py
+++ OSS_Budget/budget.py
@@ -1,9 +1,12 @@
+
+
 import datetime
 from expense import Expense
 
 class Budget:
     def __init__(self):
         self.expenses = []
+        self.monthly_limit = 500000  
 
     def add_expense(self, category, description, amount):
         today = datetime.date.today().isoformat()
@@ -24,4 +27,22 @@ class Budget:
         total = sum(e.amount for e in self.expenses)
         print(f"총 지출: {total}원\n")
 
+    def check_monthly_limit(self):
+        today = datetime.date.today()
+        current_year = today.year
+        current_month = today.month
 
+        monthly_total = sum(
+            e.amount for e in self.expenses
+            if datetime.date.fromisoformat(e.date).year == current_year and
+               datetime.date.fromisoformat(e.date).month == current_month
+        )
+
+        print(f"\n[{current_year}년 {current_month}월 지출 점검]")
+        print(f"이번 달 총 지출: {monthly_total}원")
+        print(f"지출 한도: {self.monthly_limit}원")
+
+        if monthly_total > self.monthly_limit:
+            print("2025년 6월달 예산을 초과 하였습니다. \n")
+        else:
+            print("돈을 잘 아끼고 있습니다. 부자가 됩시다.\n")
--- OSS_Budget/main.py
+++ OSS_Budget/main.py
@@ -1,19 +1,21 @@
-from budget import Budget
 
+
+from budget import Budget
 
 def main():
     budget = Budget()
 
     while True:
-        print("==== 간단 가계부 ====")
+        print("2025년 6월 가계부")
         print("1. 지출 추가")
         print("2. 지출 목록 보기")
         print("3. 총 지출 보기")
-        print("4. 종료")
+        print("4. 이번 달 예산 점검하기")
+        print("5. 종료")
         choice = input("선택 > ")
 
         if choice == "1":
-            category = input("카테고리 (예: 식비, 교통 등): ")
+            category = input("카테고리: ")
             description = input("설명: ")
             try:
                 amount = int(input("금액(원): "))
@@ -29,12 +31,14 @@ def main():
             budget.total_spent()
 
         elif choice == "4":
+            budget.check_monthly_limit()
+
+        elif choice == "5":
             print("가계부를 종료합니다.")
             break
 
         else:
             print("잘못된 선택입니다.\n")
-
 
 if __name__ == "__main__":
     main()
